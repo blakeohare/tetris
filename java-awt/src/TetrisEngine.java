6b5b25c6a95face9e071143df6b2d4786a87d144
Blake O'Hare <mspaintmaestro@gmail.com>
2021-08-06 16:10:05 -0700

    Java: Render the tetris game board (standin tiles)

--- java-awt/src/TetrisEngine.java
+++ java-awt/src/TetrisEngine.java
@@ -3,21 +3,60 @@ import java.util.List;
 public class TetrisEngine {
   
   int counter = 0;
-  
-  public TetrisEngine() {
 
+  int[][] grid;
+  int[][] overlay = null;
+
+  public TetrisEngine() {
+    this.grid = new int[10][];
+    for (int x = 0; x < 10; ++x) {
+      this.grid[x] = new int[20];
+      for (int y = 0; y < 20; ++y) {
+        this.grid[x][y] = (x + y) % 4;
+      }
+    }
   }
 
   public void update(List<String> events) {
-    
+
   }
 
   public void render(GameRenderSurface canvas) {
     this.counter++;
     if (this.counter > 255) this.counter = 0;
 
+    int tileSize = 20;
+    int boardWidth = tileSize * 10;
+    int screenWidth = 800;
+    int boardLeft = (screenWidth - boardWidth) / 2;
+    int boardHeight = tileSize * 20;
+    int screenHeight = 600;
+    int boardTop = (screenHeight - boardHeight) / 2;
+
+    int[] color1 = new int[] { 255, 255, 255 };
+    int[] color2 = new int[] { 0, 128, 255 };
+    int[] color3 = new int[] { 0, 128, 50 };
+    int[][] colors = new int[][] { 
+      null,
+      color1,
+      color2, 
+      color3,
+    };
+
     canvas
-      .fill(this.counter, 0, 255 - this.counter)
-      .rectangle(10, 20, 50, 60, 255, 255, 0);
+      .fill(40, 40, 40)
+      .rectangle(boardLeft, boardTop, boardWidth, boardHeight, 0, 0, 0);
+    
+    int colorId;
+    int[] color;
+    for (int y = 0; y < 20; ++y) {
+      for (int x = 0; x < 10; ++x) {
+        colorId = this.grid[x][y];
+        if (colorId != 0) {
+          color = colors[colorId];
+          canvas.rectangle(boardLeft + tileSize * x, boardTop + tileSize * y, tileSize, tileSize, color[0], color[1], color[2]);
+        }
+      }
+    }
   }
 }
