06c6463cfc42d3a064fcf3775fdf6defe3dfe711
Blake O'Hare <mspaintmaestro@gmail.com>
2021-08-07 13:44:43 -0700

    Go: add draw/update/fps throttle phases to the game loop

--- go-sdl/main.go
+++ go-sdl/main.go
@@ -3,18 +3,17 @@ package main
 // This is not Tetris.
 // This is just some sample boilerplate code I found for SDL2 in Go
 
-import ("fmt"
+import ("time"
         "github.com/veandco/go-sdl2/sdl")
 
 func main() {
-	fmt.Println("Hello, World!")
 
 	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
 		panic(err)
 	}
 	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	window, err := sdl.CreateWindow("Tetris", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		800, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
@@ -25,21 +24,46 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	surface.FillRect(nil, 0)
-
-	rect := sdl.Rect{0, 0, 200, 200}
-	surface.FillRect(&rect, 0xffff0000)
-	window.UpdateSurface()
 
 	running := true
+
+	const fps = 60;
+	const nspf int64 = 1000000000 / fps
+
+	var x int32 = 0
+	var y int32 = 0
+
 	for running {
+
+		frameStart := time.Now().UnixNano()
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch event.(type) {
 			case *sdl.QuitEvent:
-				println("Quit")
 				running = false
 				break
 			}
+		}
+
+		surface.FillRect(nil, 0)
+		rect := sdl.Rect{x, y, 200, 200}
+		surface.FillRect(&rect, 0xffff0000)
+		window.UpdateSurface()
+
+		x += 2
+		y += 1
+	
+		if x > 600 {
+			x = 0
+		}
+		if y > 400 {
+			y = 0
+		}
+
+		frameEnd := time.Now().UnixNano()
+		diff := frameEnd - frameStart
+		delay := nspf - diff
+		if delay > 0 {
+			time.Sleep(time.Duration(delay))
 		}
 	}
 }
