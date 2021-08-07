4f5ad98362501bea96f7589d199433eff9d9ee3b
Blake O'Hare <mspaintmaestro@gmail.com>
2021-08-07 13:23:56 -0700

    Go: boilerplate SDL2 in Go code.

--- go-sdl/main.go
+++ go-sdl/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+// This is not Tetris.
+// This is just some sample boilerplate code I found for SDL2 in Go
+
+import ("fmt"
+        "github.com/veandco/go-sdl2/sdl")
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		panic(err)
+	}
+	surface.FillRect(nil, 0)
+
+	rect := sdl.Rect{0, 0, 200, 200}
+	surface.FillRect(&rect, 0xffff0000)
+	window.UpdateSurface()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
 }
